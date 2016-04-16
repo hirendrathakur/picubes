65f5df656b103ca1506be8eb9127016efb9b08c1
hirendra <hirendrathakur1993@gmail.com>
2016-04-17 00:42:22 +0530

    update

--- src/contact/models.py
+++ src/contact/models.py
@@ -8,17 +8,10 @@ from django.contrib.auth.models import User
 class Contact(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(null=True, blank=False)
-    #phone = models.IntegerField(max_length=10,unique=True, validators=[RegexValidator(regex='^\d{10}$', message='Length has to be 10', code='Invalid number')])
-    phone = models.CharField(max_length = 10, null=True, blank=True)
-    #timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    #updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    phone = models.CharField(max_length=10, null=True, blank=False)
+    message = models.CharField(max_length = 1000, null=True, blank=True, unique=False)
 
     def __unicode__(self):
         return smart_unicode(self.email)
 
-
-    #code
-#class user(models.Model):
- #   username=models.OneToOneField(User)
-  #  password=models.CharField(max_length=100)
 
--- src/main/views.py
+++ src/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, HttpResponseRedirect, HttpResponse
 from contact.models import Contact
 # Create your views here.
 def index(request):
@@ -6,8 +6,9 @@ def index(request):
         n = request.POST.get('name')
         e = request.POST.get('email')
         p = request.POST.get('phone')
-        save_it = Contact(name=n, email=e, phone=p)
+        m = request.POST.get('message')
+        save_it = Contact(name=n, email=e, phone=p, message=m)
         save_it.save();
-        return render(request, 'index2.html')
+        return HttpResponseRedirect('/')
     else:
         return render(request, 'index2.html')
